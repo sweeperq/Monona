f39838a8cc3ad86703f1a85c3960e15812dd6ff4
sweeperq <sweeperq@gmail.com>
2018-09-27 12:09:06 -0500

    Web layout

---
+++ src/Monona.Web/Client/site.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    // focus on the first visible input
+    $('#site-body input:visible').first().focus();
+
+    // configure metismenu
+    $('#metismenu').metisMenu();
+
+    // Hide menu by default when loading on mobile
+    if ($(window).width() < 768) {
+        $('#site-nav').removeClass('show');
+    }
+
+    $('#site-nav-toggle').on('click', function (e) {
+        $('#site-nav').toggleClass('show');
+    });
+});
