21dcaa063949546769245861b7bb8f87f1874619
Sam Kimmel <sam@wingradirect.com>
2018-09-28 14:44:35 -0500

    Add fieldName sort spec

--- src/Monona.Core/Specifications/SortSpecification.cs
+++ src/Monona.Core/Specifications/SortSpecification.cs
@@ -6,6 +6,16 @@ namespace Monona.Core.Specifications
     public class SortSpecification<T>
         where T : class
     {
+        public SortSpecification(string fieldName, SortDirection direction = SortDirection.Ascending)
+        {
+            fieldName.ThrowIfEmpty(nameof(fieldName));
+            var paramExpr = Expression.Parameter(typeof(T));
+            var propExpr = Expression.Convert(Expression.PropertyOrField(paramExpr, fieldName), typeof(object));
+            var expr = Expression.Lambda<Func<T, object>>(propExpr, paramExpr);
+            Field = expr;
+            Direction = direction;
+        }
+
         public SortSpecification(Expression<Func<T,object>> field, SortDirection direction = SortDirection.Ascending)
         {
             field.ThrowIfNull(nameof(field));
